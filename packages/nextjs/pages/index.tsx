412a8ff216a869576edda73a2f8a640f868ba5b9
NeelClaudel <neel.claudel@gmail.com>
2023-10-23 15:44:12 +0200

    front:header/index-init-setup-addLogo

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
@@ -11,22 +11,22 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Nest Stablecoin</span>
           </h1>
           <p className="text-center text-lg">
-            Get started by editing{" "}
+            Get started by connecting{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
+              your wallet and Lend ETH and BTC and mint the stablecoin you need
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Enjoy High and stable earning by borrowing and stack in{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
+              Your Personnalized Vault
             </code>{" "}
             in{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
+              with high yield
             </code>
           </p>
         </div>
@@ -36,19 +36,29 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, molestias!{" "}
                 <Link href="/debug" passHref className="link">
-                  Debug Contract
+                  Here Section 1 Contract
                 </Link>{" "}
                 tab.
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Lorem, ipsum dolor.{" "}
+                <Link href="/example-ui" passHref className="link">
+                  Lorem, ipsum.
+                </Link>{" "}
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
+                Lorem ipsum dolor sit amet consectetur.{" "}
                 <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                  Lorem, ipsum dolor.
                 </Link>{" "}
                 tab.
               </p>
